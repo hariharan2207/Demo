c193322e516d49a03b8e7b22062d82c571476d37
Pasupathi021 <pasupathi@aaludra.com>
2021-10-28 11:12:33 +0530

    No chnages

--- login form validation.js
+++ login form validation.js
@@ -2,9 +2,9 @@ function form_validation() {
   let mail = document.getElementById("input1").value;
   let pass = document.getElementById("input2").value;
   let form = document.getElementById("loginForm");
-  form.addEventListener('submit',function(event){
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("submitted")
+    console.log("submitted");
   });
 
   let mail_char = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -12,11 +12,12 @@ function form_validation() {
     alert(" You have valid mail ");
   } else {
     alert(" Invalid gmail eg: demo@gmail.com ");
+    return false;
   }
   if (pass.length == 6) {
     alert(" Valid password ");
   } else {
     alert(" Password must be 6 Charcter ");
+    return false;
   }
-  
 }
