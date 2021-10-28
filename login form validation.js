6787599f02c9e9b2ddd2b78d28dd4089c0878ed1
Pasupathi021 <pasupathi@aaludra.com>
2021-10-28 13:07:30 +0530

    solve page reloading problem

--- login form validation.js
+++ login form validation.js
@@ -1,23 +1,22 @@
 function form_validation() {
+  console.log("Inside form Validation function >>>>");
   let mail = document.getElementById("input1").value;
   let pass = document.getElementById("input2").value;
   let form = document.getElementById("loginForm");
+
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("submitted");
+    console.log("Inside addEventListner");
   });
-
   let mail_char = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (mail.match(mail_char)) {
     alert(" You have valid mail ");
   } else {
     alert(" Invalid gmail eg: demo@gmail.com ");
-    return false;
   }
   if (pass.length == 6) {
     alert(" Valid password ");
   } else {
     alert(" Password must be 6 Charcter ");
-    return false;
   }
 }
