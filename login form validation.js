73fcfd08630d6a0a8758a954de1931dff0c530d5
Pasupathi021 <pasupathi@aaludra.com>
2021-10-28 09:30:22 +0530

    included addEventListener in js file to stop the autosubmit

--- login form validation.js
+++ login form validation.js
@@ -1,15 +1,22 @@
 function form_validation() {
-    let mail = document.forms["login"]["input1"].value;
-    let pass = document.forms["login"]["input2"].value;
-    let mail_char = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (mail.match(mail_char)) {
-      alert(" You have valid mail ");
-    } else {
-      alert(" Invalid gmail eg: demo@gmail.com ");
-    }
-    if (pass.length == 6) {
-      alert(" Valid password ");
-    } else {
-      alert(" Password must be 6 Charcter ");
-    }
+  let mail = document.getElementById("input1").value;
+  let pass = document.getElementById("input2").value;
+  let form = document.getElementById("loginForm");
+  form.addEventListener('submit',function(event){
+    event.preventDefault();
+    console.log("submitted")
+  });
+
+  let mail_char = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (mail.match(mail_char)) {
+    alert(" You have valid mail ");
+  } else {
+    alert(" Invalid gmail eg: demo@gmail.com ");
   }
+  if (pass.length == 6) {
+    alert(" Valid password ");
+  } else {
+    alert(" Password must be 6 Charcter ");
+  }
+  
+}
